8194061d457734748b8d1adf684ab6d505950ca4
Sergey Chernobrivenko <ser.n.che@gmail.com>
2024-07-05 21:10:52 +0100

    http

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
     <App />
   </React.StrictMode>
 );
